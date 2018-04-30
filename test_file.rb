9196c1d060770b81ecd677b6d1c06f62728324a7
Chiyo Nishio <nishio@c-fo.com>
2018-04-30 16:35:00 +0900

    Remove test file

--- test_file.rb
+++
@@ -1,8 +0,0 @@
-# this is a random code file for testing purposes
-
-grapefruits_are_good = false
-anotherVariableWithAnIrelevantName = true
-
-if grapefruits_are_good
-  p "grapefruits are soooooo good"
-end
